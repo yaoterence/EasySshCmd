77abf7ef9476fbe3f96800a44ff6a66a64aa7dcd
Terence Yao <terenceyao@gmail.com>
2023-11-23 21:44:12 +0800

    mount.sh support none-password setting case

--- mount.sh
+++ mount.sh
@@ -6,6 +6,10 @@ MNT_DIR=mnt/${REMOTE_ADDR}_${REMOTE_PORT}
 if [ ! -d "$MNT_DIR" ]; then
     mkdir -p $MNT_DIR
 fi
-echo $PASS | sshfs -p $REMOTE_PORT -o password_stdin $SSH_ALGO_CMD $I_CMD $USER@$REMOTE_ADDR:$REMOTE_PATH $MNT_DIR
+if [[ -z "$PASS" ]] ; then
+    sshfs -p $REMOTE_PORT $SSH_ALGO_CMD $I_CMD $USER@$REMOTE_ADDR:$REMOTE_PATH $MNT_DIR
+else
+    echo $PASS | sshfs -p $REMOTE_PORT -o password_stdin $SSH_ALGO_CMD $I_CMD $USER@$REMOTE_ADDR:$REMOTE_PATH $MNT_DIR
+fi
 echo $MNT_DIR
 ls -la $MNT_DIR
